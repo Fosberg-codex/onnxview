8eee260c0eca8381a60b9fa6e3f3e59c33de06ed
Fosberg-codex <fosberg1addai@gmail.com>
2024-07-15 22:01:58 +0000

    minor change

--- app/mlmodels/page.tsx
+++ app/mlmodels/page.tsx
@@ -3,7 +3,7 @@ import ModelList from "../components/Modellist"
 export default function Models() {
   return (
     <div>
-      <h1 className="text-3xl font-bold mb-8">Models</h1>
+      <h1 className="text-3xl font-bold mb-8">Model bay</h1>
       <ModelList />
     </div>
   )
