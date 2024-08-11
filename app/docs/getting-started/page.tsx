fa80ed767b3f24c28c8e5fcb371bb058edc12754
Fosberg Addai <83887297+Fosberg-codex@users.noreply.github.com>
2024-08-11 12:20:03 +1200

    writing chnages

--- app/docs/getting-started/page.tsx
+++ app/docs/getting-started/page.tsx
@@ -16,7 +16,7 @@ export default function GettingStartedPage() {
             <li>Enter unseen features on the feature inout fields to get your response</li>
           </ol>
           <p className="mb-4">
-            For more detailed instructions on using PlutoFlow Experimentl, contact <Link href="mailto:fosberg1addai@gmil.com" className="underline text-pink-500">Fosberg.</Link>
+            For more detailed instructions on using PlutoFlow Experimental, contact <Link href="mailto:fosberg1addai@gmil.com" className="underline text-pink-500">Fosberg.</Link>
             <Link href="/docs/web-app-usage" className="text-blue-600 hover:underline ml-1">
               Web App Usage Guide
             </Link>.
