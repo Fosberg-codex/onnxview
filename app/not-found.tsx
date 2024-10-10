1a977aa46e279f99dcd993914130faab47a9df7c
Fosberg Addai <fosberg1addai@gmail.com>
2024-10-10 20:19:42 +1300

    minor bug fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,14 +6,14 @@ export default function Notfound() {
         <div className="flex flex-col justify-center items-center">
             <div>
             <Image
-              src='/Baselogo.png'
+              src='/plutoflow.png'
               alt='Youtube video'
               width={55}
               height={55}
             />
             </div>
             <div className="my-2 text-center text-sm lg:text-base md:text-base">This page is not ready</div>
-            <Link href="/"><div className="text-center rounded-md bg-cream border border-green px-2 py-1 mt-2 cursor-pointer hover:bg-creams lg:text-base md:text-base">Go back Home</div></Link>
+            <Link href="/"><div className="text-center rounded-md bg-blue-900 text-white border border-green px-2 py-1 mt-2 cursor-pointer hover:bg-blue-900 lg:text-base md:text-base">Go back Home</div></Link>
 
         </div>
     </div>;
