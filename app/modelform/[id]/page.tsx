8ca21193991fb6272b4df35ae16964fb412a13e0
Fosberg Addai <fosberg1addai@gmail.com>
2024-10-22 13:07:08 -1100

    made small changes

--- app/modelform/[id]/page.tsx
+++ app/modelform/[id]/page.tsx
@@ -3,7 +3,7 @@ import ClientPage from '@/app/components/idpage'
 
 async function getFormDetails(id: string) {
   try {
-    const res = await fetch(`https://plutofloww.vercel.app/api/createform/${id}`, { cache: 'no-cache' });
+    const res = await fetch(`https://plutofloww.com/api/createform/${id}`, { cache: 'no-cache' });
     if (!res.ok) throw new Error('Failed to fetch form details');
     return await res.json();
   } catch (err: any) {
